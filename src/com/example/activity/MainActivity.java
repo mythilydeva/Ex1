dc2dffe4cd5868eb825727ae6fde1dedec4d2761
ITLAB <ITLAB@IT212>
2017-01-09 14:39:54 +0530

    last commit

--- src/com/example/activity/MainActivity.java
+++ src/com/example/activity/MainActivity.java
@@ -11,34 +11,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends ActionBarActivity {
 	Button helBut;
 	TextView textHel;
-	//EditText textInput;
 	EditText textInput2;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        helBut = (Button) findViewById(R.id.b1);
-        textInput2 = (EditText) findViewById(R.id.text_2);
-        helBut.setOnClickListener(new View.OnClickListener() {
-			
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		helBut = (Button) findViewById(R.id.b1);
+		textInput2 = (EditText) findViewById(R.id.text_2);
+		helBut.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View arg0) {
 				String data1 = textInput2.getText().toString();
-				/*int a=Integer.parseInt(data)+Integer.parseInt(data1);
-				String m=Integer.toString(a);
-				Toast msg = Toast.makeText(getApplicationContext(), data,Toast.LENGTH_LONG);
-				msg.show();
-				textHel.setText("Hai "+data1);*/
-				Intent i = new Intent(MainActivity.this,ScndActivity.class);
-				i.putExtra("username",data1);
+				Intent i = new Intent(MainActivity.this, ScndActivity.class);
+				i.putExtra("username", data1);
 				startActivity(i);
-				
+
 			}
 		});
-    }
+	}
 
 }
--- src/com/example/activity/ScndActivity.java
+++ src/com/example/activity/ScndActivity.java
@@ -1,36 +1,38 @@
 package com.example.activity;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class ScndActivity extends Activity{
+public class ScndActivity extends Activity {
 	TextView result;
 	TextView result1;
 	TextView result2;
-	
-	
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
-		 super.onCreate(savedInstanceState);
-		 setContentView(R.layout.activity_scnd);
-		 result = (TextView) findViewById(R.id.text2);
-		 result1 = (TextView) findViewById(R.id.text3);
-		 result2 = (TextView) findViewById(R.id.text4);
-		 Typeface tfont=Typeface.createFromAsset(getAssets(), "AlexBrush-Regular.ttf");
-		 Typeface t1font=Typeface.createFromAsset(getAssets(), "Amble-Italic.ttf");
-		 Typeface t2font=Typeface.createFromAsset(getAssets(), "KaushanScript-Regular.otf");
-		 Intent i = getIntent();
-		 String datafrmMain = i.getStringExtra("username");
-		 result.setTypeface(tfont);
-		 result.setText("Hai "+datafrmMain);
-		 result1.setTypeface(t1font);
-		 result1.setText("Hello "+datafrmMain);
-		 result2.setTypeface(t2font);
-		 result2.setText("Welcome "+datafrmMain);
-		        
-		
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scnd);
+		result = (TextView) findViewById(R.id.text2);
+		result1 = (TextView) findViewById(R.id.text_3);
+		result2 = (TextView) findViewById(R.id.text4);
+		Typeface tfont = Typeface.createFromAsset(getAssets(),
+				"AlexBrush-Regular.ttf");
+		Typeface t1font = Typeface.createFromAsset(getAssets(),
+				"DeliusSwashCaps-Regular.ttf");
+		Typeface t2font = Typeface.createFromAsset(getAssets(),
+				"KaushanScript-Regular.otf");
+		Intent i = getIntent();
+		String datafrmMain = i.getStringExtra("username");
+		result.setTypeface(tfont);
+		result.setText("Hai " + datafrmMain);
+		result1.setTypeface(t1font);
+		result1.setText("Hello " + datafrmMain);
+		result2.setTypeface(t2font);
+		result2.setText("Welcome " + datafrmMain);
+
 	}
 
 }
