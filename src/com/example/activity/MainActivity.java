b20e3805e6b9b572432a1c3bcf1773a86bac37d1
ITLAB <ITLAB@IT212>
2017-01-09 13:53:09 +0530

    First Commit

---
+++ src/com/example/activity/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+	Button helBut;
+	TextView textHel;
+	//EditText textInput;
+	EditText textInput2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        helBut = (Button) findViewById(R.id.b1);
+        //textHel = (TextView) findViewById(R.id.textView);
+        //textInput = (EditText) findViewById(R.id.text1);
+        textInput2 = (EditText) findViewById(R.id.text_2);
+        helBut.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				//textHel.setText("Hai");
+				
+				//String data = textInput.getText().toString();
+				String data1 = textInput2.getText().toString();
+				//int a=Integer.parseInt(data)+Integer.parseInt(data1);
+				//String m=Integer.toString(a);
+				//Toast msg = Toast.makeText(getApplicationContext(), data,Toast.LENGTH_LONG);
+				//msg.show();
+				//textHel.setText("Hai "+data1);
+				Intent i = new Intent(MainActivity.this,ScndActivity.class);
+				i.putExtra("username",data1);
+				startActivity(i);
+				
+			}
+		});
+    }
+
+}
